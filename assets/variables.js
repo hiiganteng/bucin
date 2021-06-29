f51d225b1f0217ee632e7344d17dc02bece17102
hiiganteng <84143312+hiiganteng@users.noreply.github.com>
2021-06-29 23:29:55 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,7 +1,7 @@
 export default {
   nicknames: ["Nendennnn"],
   greetings: {
-    evening: "Selamat Pagi",
+    evening: "Selamat Malam",
     afternoon: "Selamat Sore",
     day: "Selamat Siang",
     morning: "Selamat Malam",
